658396a4a9c2462cb0a35c4598ac36b9049ceefd
Sethgood7 <sethgooodman657@gmail.com>
2023-08-30 19:26:32 -0700

    delete functionality implemented

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const methodOverride = require('method-override')
 
 const port = process.env.PORT || 8080
 
@@ -10,7 +11,7 @@ app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
-
+app.use(methodOverride('_method'))
 
 // controllers and routes
 app.use('/places', require('./controllers/places'))
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,14 +1,33 @@
 const React = require('react')
 const Def = require('../default')
 
-function show () {
+function show (data) {
     return (
         <Def>
           <main>
-            <h1>Show Page</h1>
+            <h1>{ data.place.name }</h1>
+            <p className="text-center"><h2>Cuisines</h2>{data.place.cuisines}</p>
+            <img src={data.place.pic} alt={data.place.name} />
+            <p className="text-center"><h2>Located in</h2>{data.place.city}, {data.place.state}</p>
+            <div>
+                <h2>Ratings:</h2>
+                <p>currently unrated</p>
+            </div>
+            <div>
+                <h2>Comments:</h2>
+                <p>currently no comments</p>
+            </div>
+            <div>
+            <a href={`/places/${data.id}/edit`} className="btn btn-warning">Edit</a>     
+            <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+                <button type="submit" className="btn btn-danger">Delete</button>
+            </form> 
+ 
+            </div>
           </main>
         </Def>
     )
 }
 
 module.exports = show
+
