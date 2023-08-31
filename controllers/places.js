d36f90eb47312ed3a06c004fdefee9dd3089b43c
Sethgood7 <sethgooodman657@gmail.com>
2023-08-31 12:16:27 -0700

    added show page and routes to update the restaurant, not working

--- controllers/places.js
+++ controllers/places.js
@@ -1,14 +1,15 @@
 const router = require('express').Router()
 const places = require('../models/places-model.js')
 
+// places get
 router.get('/', (req, res) => {
     res.render('places/index', { places })
 })
-
+//new places get
   router.get('/new', (req, res) => {
     res.render('places/new')
   })
-
+// places post
   router.post('/', (req, res) => {
     if (!req.body.pic) {
       // Default image if one is not provided
@@ -23,7 +24,40 @@ router.get('/', (req, res) => {
     places.push(req.body)
     res.redirect('/places')
   })
-   
+
+    // edit route
+    router.get('/:id/edit', (req, res) => {
+      let id = Number(req.params.id)
+      if (isNaN(id)) {
+          res.render('error404')
+      }
+      else if (!places[id]) {
+          res.render('error404')
+      }
+      else {
+        res.render('places/edit', { place: places[id] })
+      }
+    })
+
+    router.put('/:id', (req, res) => {
+      console.log('PUT request revieved for ID:', req.params.id);
+      let id = Number(req.params.id);
+      if (isNaN(id) || !places[id]) {
+          return res.render('error404');
+      }
+  
+      // Assuming you are storing places as objects in an array
+      // and that req.body contains the updated place data.
+      places[id] = {
+          ...places[id], 
+          ...req.body
+      };
+  
+      res.redirect(`/places/${id}`);
+
+  });
+
+   //making sure id is a number
   router.get('/:id', (req, res) => {
     let id = Number(req.params.id)
     if (isNaN(id)) {
@@ -36,7 +70,7 @@ router.get('/', (req, res) => {
       res.render('places/show', { place: places[id], id })
     }
   })
-  
+  // delete route
   router.delete('/:id', (req, res) => {
     let id = Number(req.params.id)
     if (isNaN(id)) {
@@ -50,6 +84,6 @@ router.get('/', (req, res) => {
       res.redirect('/places')
     }
   })
-  
+
 
 module.exports = router
